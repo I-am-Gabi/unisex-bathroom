e4830f16586599f0ac82b694766ca3b351e96c56
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-05-20 20:18:31 -0300

    - add gender - add bathroom and person - remove bathroom line

---
+++ src/concorrentes/imd/ufrn/Gender.java
@@ -0,0 +1,18 @@
+package concorrentes.imd.ufrn;
+
+import java.util.Random;
+
+/**
+ * Gender enum.
+ * 
+ * @author  Gabriela Cavalcante and Irene Ginani
+ * @version 20/05/2018
+ */
+public enum Gender {
+	Woman, Men;
+
+	public static Gender getGender()  {
+		Random random = new Random();
+	    return values()[random.nextInt(values().length)];
+	}
+}
--- src/concorrentes/imd/ufrn/Person.java
+++ src/concorrentes/imd/ufrn/Person.java
@@ -1,16 +1,42 @@
 package concorrentes.imd.ufrn;
 
 public class Person extends Thread {
-	private BathroomLine line;
+	private Gender gender;
+	private String name;
+	private Bathroom bathroom;
 	
-	public Person(String name, BathroomLine line) {
-		super(name);
-		this.line = line;
+	public Person(String name, Bathroom bathroom) {
+		super(name); 
+		this.bathroom = bathroom;
+		this.gender = Gender.getGender();
 	}
 	
 	@Override
-	public void run() {
-		System.out.println("Get in the bathroom... " + Thread.currentThread().getName());
-		line.accessBathroom();
+	public void run() { 
+		while(true) {  
+			try {
+				Thread.sleep ((long) (500));
+			} catch (InterruptedException e) { 
+				e.printStackTrace();
+			}
+
+			bathroom.getin(this);
+			
+			synchronized(bathroom){
+				try {
+					// Wait until can get in
+					bathroom.wait();	
+					System.out.println("Get in the bathroom... " + Thread.currentThread().getName());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} 
+			
+			bathroom.getout(this);
+		}
 	}
+
+	public Gender getGender() {
+		return gender;
+	} 
 }
