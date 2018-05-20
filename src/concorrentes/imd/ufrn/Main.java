123b1484d7de87f5d1b2ac6cb51f0a15d956d27f
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-05-20 20:18:31 -0300

    unisex bathroom #1
    - add gender
    - add bathroom and person
    - remove bathroom line

--- src/concorrentes/imd/ufrn/Main.java
+++ src/concorrentes/imd/ufrn/Main.java
@@ -1,13 +1,28 @@
 package concorrentes.imd.ufrn;
 
+/**
+ * 
+ * @author Gabriela Cavalcante and Irene Ginani
+ * @version 20/05/2018
+ */
 public class Main {
 
 	public static void main(String[] args) {  
-		BathroomLine line = new BathroomLine();
+		Bathroom bathroom = new Bathroom();
 	    Person[] processos = new Person[10];
-	    for (int i = 0; i < 10; i++) {
-	        processos[i] = new Person("person " + i, line);
+	    
+	    for (int i = 0; i < 10; i++) { 
+	        processos[i] = new Person("person " + i, bathroom); 
 	        processos[i].start();
+	    }
+	    
+	    for (int i = 0; i < 10; i++) {   
+	        try {
+				processos[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	    }
 	} 
 }
